6b9e5fd5d39c6687c4a89c3629e374ef2d924769
Bandla Guruprasad <131840350+BANDLA-BOT@users.noreply.github.com>
2024-08-02 15:41:32 +0530

    Express server Setup

---
+++ server/dist/index.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var app = (0, _express["default"])();
+app.listen(8000, function () {
+  console.log('Server running on Port 3000');
+});
+//# sourceMappingURL=index.dev.js.map
