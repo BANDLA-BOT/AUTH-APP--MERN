64e8984ec840916b28126af426ff96220b7f32b0
Bandla Guruprasad <131840350+BANDLA-BOT@users.noreply.github.com>
2024-08-02 16:14:32 +0530

    MongoDb Connected

--- server/dist/index.dev.js
+++ server/dist/index.dev.js
@@ -2,7 +2,20 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_dotenv["default"].config(); //DATABASE
+
+
+_mongoose["default"].connect(process.env.MONGO).then(function () {
+  console.log('Connected to the Database');
+})["catch"](function (err) {
+  console.log(err.message);
+});
 
 var app = (0, _express["default"])();
 app.listen(8000, function () {
--- server/index.js
+++ server/index.js
@@ -1,4 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+//DATABASE
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to the Database')
+})
+.catch((err)=>{
+    console.log(err.message)
+})
 
 const app = express()
 
